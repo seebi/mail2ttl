2590832775ca38ee221ba37e20cd527705bd6eed
Lukas Eipert <git@leipert.io>
2016-08-16 14:13:29 +0200

    initial commit

---
+++ generateMail.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var path = require('path');
+
+
+var email = "From: 'Sender Name' <sender@example.com>\r\n"+
+    "To: Receiver Name <receiver@example.com>\r\n"+
+    "Subject: Hello world!\r\n"+
+    "\r\n"+
+    "How are you today?";
+
+fs.writeFileSync(path.join(__dirname, 'mail.file'), email);
