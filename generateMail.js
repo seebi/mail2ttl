c5826641ab00bf2a251f708b41700e9a746c82cc
Lukas Eipert <git@leipert.io>
2016-08-16 14:19:04 +0200

    some comments

--- generateMail.js
+++ generateMail.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var path = require('path');
 
 
-var email = "From: 'Sender Name' <sender@example.com>\r\n"+
-    "To: Receiver Name <receiver@example.com>\r\n"+
+var email = "From: Sender Name <sender@example.com>\r\n"+
+    "To: Receiver Name <receiver@example.com>, <receiver2@example.com>\r\n"+
     "Subject: Hello world!\r\n"+
     "\r\n"+
     "How are you today?";
--- index.js
+++ index.js
@@ -8,21 +8,27 @@ function md5(data) {
     return crypto.createHash('md5').update(data).digest('hex');
 }
 
+//prints a person
 function printPerson(person) {
     console.log(':' + md5(person.address), 'a', 'foaf:person', ';');
-    console.log('\t', 'foaf:name', '"' + person.name + '"', ';')
-    console.log('\t', 'foaf:mbox', person.address, '.')
+    if(person.name) {
+        console.log('\t', 'foaf:name', '"' + person.name + '"', ';');
+    }
+    console.log('\t', 'foaf:mbox', person.address, '.');
 }
 
+// print ttl
 function mail2ttl(mailObject) {
     // See here for all the things mailparser returns https://www.npmjs.com/package/mailparser#parsed-mail-object
 
     mailObject.to.forEach(printPerson);
+    mailObject.from.forEach(printPerson);
 
     // console.log('Subject:', mailObject.subject); // Hello world!
     // console.log('Text body:', mailObject.text); // How are you today?
 }
 
+// parse the mail file
 function parseMail(path) {
     mailparser.on('end', mail2ttl);
     fs.createReadStream(path).pipe(mailparser);
